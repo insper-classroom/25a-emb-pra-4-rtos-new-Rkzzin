6c7f9e3918f3be30d9521873d2ae6bd3465f6eb4
Rkzzin <henriquels5@al.insper.edu.br>
2025-03-24 09:59:13 -0300

    should work

--- main/main.c
+++ main/main.c
@@ -12,25 +12,25 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const uint BTN_1_OLED = 28;
-const uint BTN_2_OLED = 26;
-const uint BTN_3_OLED = 27;
-const uint LED_1_OLED = 20;
-const uint LED_2_OLED = 21;
-const uint LED_3_OLED = 22;
 const uint TRIG_PIN = 17;
 const uint ECHO_PIN = 16;
 
-volatile uint64_t time_init = 0;
-volatile uint64_t time_end = 0;
+QueueHandle_t xQueueTime;               // Fila com informação do tempo to_us_since_boot
+QueueHandle_t xQueueDistance;           // Valor da distância em cm lido pela task_echo
+SemaphoreHandle_t xSemaphoreTrigger;    // Avisa o OLED que uma leitura foi disparada
+
 
 void pin_callback(uint gpio, uint32_t events) {
+    uint64_t time;
     if (events == 0x4) {
-        time_end = to_us_since_boot(get_absolute_time());
+        time = to_us_since_boot(get_absolute_time());
     } else if (events == 0x8) {
-        time_init = to_us_since_boot(get_absolute_time());
+        time = to_us_since_boot(get_absolute_time());
     }
+
+    xQueueSendFromISR(xQueueTime, &time, 0);
 }
+
 
 void trigger_task (void *p) {
     gpio_init(TRIG_PIN);
@@ -40,6 +40,7 @@ void trigger_task (void *p) {
         gpio_put(TRIG_PIN, 1);
         sleep_us(10);
         gpio_put(TRIG_PIN, 0);
+        xSemaphoreGive(xSemaphoreTrigger);
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
@@ -49,14 +50,21 @@ void echo_task (void *p) {
     gpio_set_dir(ECHO_PIN, GPIO_IN);
     gpio_set_irq_enabled_with_callback(ECHO_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &pin_callback);
 
-    while (1) {
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
+    uint64_t time_init = 0;
+    uint64_t time_end;
 
-float get_distance() {
-    int duration = time_end - time_init;
-    return (duration * 0.034) / 2.0;
+    while (1) {
+        if (xQueueReceive(xQueueTime, &time_init, pdMS_TO_TICKS(100))) {
+            xQueueReceive(xQueueTime, &time_end, pdMS_TO_TICKS(100));
+
+            float duration = (float)(time_end - time_init);
+            float distance = (duration * 0.034) / 2;
+
+            printf("Distancia: %.2f cm\n", distance);
+
+            xQueueSend(xQueueDistance, &distance, pdMS_TO_TICKS(100));
+        }
+    }
 }
 
 void oled_task (void *p) {
@@ -64,29 +72,45 @@ void oled_task (void *p) {
     ssd1306_t disp;
     gfx_init(&disp, 128, 32);
 
+    float dist;
+    char dist_str[10];
+
     while (1) {
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 1, "Distancia: ");
-        gfx_draw_string(&disp, 0, 10, 1, "cm");
-        float dist = get_distance();
-        char dist_str[10];
-        sprintf(dist_str, "%.2f", dist);
-        gfx_draw_string(&disp, 0, 20, 1, dist_str);
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(100));
+        if (xSemaphoreTake(xSemaphoreTrigger, pdMS_TO_TICKS(100)) == pdTRUE) {
+            if (xQueueReceive(xQueueDistance, &dist, pdMS_TO_TICKS(100))) {
+                gfx_clear_buffer(&disp);
+                gfx_draw_string(&disp, 0, 0, 1, "Distancia: ");
+                gfx_draw_string(&disp, 0, 10, 1, "cm");
+                
+                if (dist > 300) {
+                    gfx_draw_string(&disp, 0, 20, 1, "Fora de alcance");
+                } else {
+                    sprintf(dist_str, "%.2f", dist);
+                    gfx_draw_string(&disp, 0, 20, 1, dist_str);
+                }
+                
+                gfx_show(&disp);
+            } else {
+                gfx_clear_buffer(&disp);
+                gfx_draw_string(&disp, 0, 0, 1, "Fio Desconectado");
+                gfx_show(&disp);
+            }
+        } 
     }
 }
 
 int main() {
     stdio_init_all();
 
-    
+    xSemaphoreTrigger = xSemaphoreCreateBinary();
+    xQueueTime = xQueueCreate(32, sizeof(uint64_t));
+    xQueueDistance = xQueueCreate(32, sizeof(float));
+
     xTaskCreate(trigger_task, "Trigger", 4095, NULL, 1, NULL);
     xTaskCreate(echo_task, "Echo", 4095, NULL, 1, NULL);
     xTaskCreate(oled_task, "OLED", 4095, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
-    while (true)
-        ;
+    while (true) {}
 }
